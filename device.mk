33f03acce3de858d827c0e27a531e3a484c4c5f2
Haky86 <hakamelassouad@gmail.com>
2025-03-29 22:03:00 +0100

    a71: Drop AOSP NFC service
    * Our device is using proprietary NFC service.
    
    Change-Id: I0443c222494af35bb84ab588f48e942aa2a3694e

--- device.mk
+++ device.mk
@@ -17,9 +17,6 @@ PRODUCT_COPY_FILES += \
 TARGET_HAS_UDFPS := true
 
 # NFC
-PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.2-service \
-
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/rootdir/vendor/etc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
     $(DEVICE_PATH)/rootdir/vendor/etc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf \
