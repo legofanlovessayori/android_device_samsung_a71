2c1c5b2f63ef8aeabf44657e36bbf154a7a83aa0
Haky86 <hakamelassouad@gmail.com>
2025-04-29 11:01:23 +0200

    a71: Correct TARGET_BOARD_NAME
    Change-Id: I2e88c70027b4bd37f1a510edd10ce7d76f13ed70

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,7 +12,7 @@ DEVICE_PATH := device/samsung/a71
 TARGET_OTA_ASSERT_DEVICE := a71
 
 # Board
-TARGET_BOARD_NAME := SRPSF18B010
+TARGET_BOARD_NAME := SRPSF18B011
 
 # FOD
 $(call soong_config_set,samsung_udfps,udfps_zorder,0x20000000u)
